d1d8692be47a3b5ef6b87e6bd12b5a645279f700
unknown <louissilva0899@protonmail.com>
2023-09-04 11:51:46 +0100

    Added HexType object

--- map_gen/scripts/catan_board/hex.py
+++ map_gen/scripts/catan_board/hex.py
@@ -1,5 +1,10 @@
+from typing import Optional
+
 from coords import Coords
+from hex_type import HexType
+
 
 class Hex:
-    def __init__(self, coords: Coords) -> None:
-        pass
+    def __init__(self, coords: Coords, resource: Optional[HexType] = None, probability: Optional[int] = None) -> None:
+        self.coords = coords
+
--- map_gen/scripts/catan_board/hex_type.py
+++ map_gen/scripts/catan_board/hex_type.py
@@ -1,5 +1,41 @@
-from typing import List
+class ResourceUnknown(Exception):
+    pass
+
 
 class HexType:
-    def __init__(self, type: str) -> None:
-        pass
+    """An object that represents the resource type of a hex"""
+
+    def __init__(self, resource: str) -> None:
+        # Check if the inputted type is valid
+        self.set_resource(resource)
+        
+    def __str__(self) -> str:
+        return self.__resource
+
+    @property
+    def resource(self) -> str:
+        return self.__str__()
+    
+    def set_resource(self, resource: str) -> None:
+        resource = resource.lower()
+        
+        if resource == "s" or resource == "sheep":
+            self.__resource = "sheep"
+
+        elif resource == "w" or resource == "wood":
+            self.__resource = "wood"
+
+        elif resource == "o" or resource == "ore":
+            self.__resource = "ore"
+
+        elif resource == "g" or resource == "grain":
+            self.__resource = "grain"
+
+        elif resource == "b" or resource == "brick":
+            self.__resource = "brick"
+
+        elif resource == "d" or resource == "desert":
+            self.__resource = "desert"
+
+        else:
+            raise ResourceUnknown(f"The resource inputted: '{resource}' is unknown.")
